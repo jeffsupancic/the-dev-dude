746af15b9c35c31b5871491c2e2c82f1ca9f38fd
jeffsupancic <jsupancic@gmail.com>
2021-10-24 12:57:36 -0700

    update blog

--- src/data/blogEntries.js
+++ src/data/blogEntries.js
@@ -1,6 +1,6 @@
 const blogEntries = [
-  { id: 1, date: "10/24/21", title: "Test 1", body: "Body 1" },
-  { id: 2, date: "", title: "Test 2", body: "Body 2" },
+  { id: 1, date: "10/24/21", title: "Create React App", body: "TODO" },
+  { id: 2, date: "", title: "Hosting with Digital Ocean", body: "TODO" },
 ];
 
 export default blogEntries;
