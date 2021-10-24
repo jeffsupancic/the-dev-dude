0a7cfa759c24773cb5db960478b83f0f360243c3
jeffsupancic <jsupancic@gmail.com>
2021-10-24 09:39:54 -0700

    add react router with a few test pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = lazy(() => import("./routes/Home"));
+const Blog = lazy(() => import("./routes/Blog"));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/blog" component={Blog} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default App;
---
+++ src/routes/Blog.tsx
@@ -0,0 +1,21 @@
+const Blog = () => {
+  return (
+    <div>
+      {`
+      Steps followed to setup this personal portfolio:
+      1. followed create react app as a starter https://create-react-app.dev/ using the type script template 
+      2. hosted with digital ocean apps which easily wraps this app in a contianer and created a yaml file for you https://www.digitalocean.com/
+      3. added react router for code splitting
+
+
+      TODO:
+      1. add more lint rules
+      2. configure prettier
+      3. enforce tests and lint rules with husky
+      4. add material ui
+      `}
+    </div>
+  );
+};
+
+export default Blog;
