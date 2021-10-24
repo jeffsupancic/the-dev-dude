e3873dee25b1c209ef82db1c360e3eade45ee6f1
jeffsupancic <jsupancic@gmail.com>
2021-10-24 10:42:42 -0700

    add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 
 // components
 import Header from "./components/layout/Header";
 const Home = lazy(() => import("./routes/Home"));
 const Blog = lazy(() => import("./routes/Blog"));
 
+// const theme = createTheme();
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 const App = () => (
-  <>
+  <ThemeProvider theme={darkTheme}>
     <CssBaseline />
     <Header />
     <Router>
@@ -19,7 +27,7 @@ const App = () => (
         </Switch>
       </Suspense>
     </Router>
-  </>
+  </ThemeProvider>
 );
 
 export default App;
