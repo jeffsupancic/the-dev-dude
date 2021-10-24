8d80fc03c61e5031a5ed96fcc346e150e5e3ec71
jeffsupancic <jsupancic@gmail.com>
2021-10-24 09:54:34 -0700

    install mui and add a test button

--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -3,9 +3,10 @@ const Blog = () => {
     <div>
       {`
       Steps followed to setup this personal portfolio:
-      1. followed create react app as a starter https://create-react-app.dev/ using the type script template 
-      2. hosted with digital ocean apps which easily wraps this app in a contianer and created a yaml file for you https://www.digitalocean.com/
+      1. followed create react app as a starter https://create-react-app.dev/ using the type script template chose to use npm
+      2. hosted with digital ocean apps which easily wraps this app in a contianer and created a yaml file for you https://www.digitalocean.com/ with automatic deploy on commit to main branch
       3. added react router for code splitting
+      4. added mui v5 didn't use styled-components
 
 
       TODO:
@@ -13,6 +14,9 @@ const Blog = () => {
       2. configure prettier
       3. enforce tests and lint rules with husky
       4. add material ui
+      5. show some data fetching
+      6. add .env variable
+      7. add / fix unit tests on github / site
       `}
     </div>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,21 @@
+import Button from "@mui/material/Button";
+import { useHistory } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home...</div>;
+  const history = useHistory();
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          history.push("/blog");
+        }}
+        variant="contained"
+      >
+        Take me to the blog
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
