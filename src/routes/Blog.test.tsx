d5b33fedf4c0e89dd39f2ceb9dbb2e14c0fad9c3
jeffsupancic <jsupancic@gmail.com>
2021-11-08 17:25:29 -0800

    add MockedProvider for Blog test

--- src/routes/Blog.test.tsx
+++ src/routes/Blog.test.tsx
@@ -2,8 +2,14 @@
 import { render } from "@testing-library/react";
 // dd
 import Blog from "./Blog";
+// apollo
+import { MockedProvider } from "@apollo/client/testing";
 
 test("Blog component renders", () => {
-  const { container } = render(<Blog />);
+  const { container } = render(
+    <MockedProvider>
+      <Blog />
+    </MockedProvider>
+  );
   expect(container.firstChild).toBeTruthy();
 });
