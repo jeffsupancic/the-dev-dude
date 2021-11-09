707084c409499c79f0245c1556098276745f9a23
jeffsupancic <jsupancic@gmail.com>
2021-11-08 17:11:16 -0800

    add apollo client and fetch blog entries

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CircularProgress from "@mui/material/CircularProgress";
+// apollo
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // components
 import Header from "./components/layout/Header";
 const Home = lazy(() => import("./routes/Home"));
 const Blog = lazy(() => import("./routes/Blog"));
 
+// theme
 // const theme = createTheme();
 const darkTheme = createTheme({
   palette: {
@@ -19,34 +22,42 @@ const darkTheme = createTheme({
   },
 });
 
+// apollo
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_SERVER,
+  cache: new InMemoryCache(),
+});
+
 const App = () => (
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-    <Router>
-      <Header />
-      <Box p={1}>
-        <Suspense
-          fallback={
-            <Box
-              p={1}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <CircularProgress size={100} />
-            </Box>
-          }
-        >
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/blog" component={Blog} />
-          </Switch>
-        </Suspense>
-      </Box>
-    </Router>
-  </ThemeProvider>
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Header />
+        <Box p={1}>
+          <Suspense
+            fallback={
+              <Box
+                p={1}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <CircularProgress size={100} />
+              </Box>
+            }
+          >
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/blog" component={Blog} />
+            </Switch>
+          </Suspense>
+        </Box>
+      </Router>
+    </ThemeProvider>
+  </ApolloProvider>
 );
 
 export default App;
--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -3,14 +3,32 @@ import { useEffect, useState } from "react";
 // mui
 import Grid from "@mui/material/Grid";
 // dd
-import blogEntriesData from "data/blogEntries";
 import BlogEntry from "components/blog/BlogEntry";
 import BlogCategorySelect from "components/blog/BlogCategorySelect";
 import BlogSortOrderSelect from "components/blog/BlogSortOrderSelect";
 // utils
 import { cloneDeep } from "lodash";
+// apollo
+import { gql, useQuery } from "@apollo/client";
+
+const BLOG_ENTRIES = gql`
+  query getBlogEntries {
+    blogEntries {
+      id
+      categories
+      date
+      title
+      body
+      code
+    }
+  }
+`;
 
 const Blog = () => {
+  // queries
+  const { data: blogEntriesData } = useQuery(BLOG_ENTRIES);
+
+  // state
   const [blogEntries, setBlogEntries] = useState<
     {
       id: number;
@@ -29,18 +47,21 @@ const Blog = () => {
     "javaScript",
   ]);
 
+  // lifecycles
   useEffect(() => {
-    let newBlogEntries = cloneDeep(blogEntriesData);
+    if (!blogEntriesData?.blogEntries) return;
+
+    let newBlogEntries = cloneDeep(blogEntriesData.blogEntries);
     // apply filter
-    newBlogEntries = newBlogEntries.filter((be) =>
-      be.categories.some((c) => categories.includes(c))
+    newBlogEntries = newBlogEntries.filter((be: { categories: string[] }) =>
+      be.categories.some((c: string) => categories.includes(c))
     );
     // apply sort
     if (sortOrder === "newestFirst") {
       newBlogEntries = newBlogEntries.reverse();
     }
     setBlogEntries(newBlogEntries);
-  }, [sortOrder, categories]);
+  }, [blogEntriesData, sortOrder, categories]);
 
   return (
     <Grid container spacing={1}>
